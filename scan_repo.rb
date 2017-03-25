e53ac169a8926f35699a70c52118c64c5ae208ff
Diego Gutierrez <dgutierrez1287@gmail.com>
2017-03-24 21:31:29 -0400

    MODIFIED: scan_repo.rb
    cleaned up spacing and applied ruby naming conventions to method names

--- scan_repo.rb
+++ scan_repo.rb
@@ -6,28 +6,35 @@ $logger.level = Logger::INFO
 
 $top_dir = "/repo"
 
-def scanAndUpdate()
+##
+# scan_and_update
+# this will go through all the directories containing .rpm files
+# and will run a createrepo that will update the yum repo metadata
+##
+def scan_and_update
 
-        # get repo dir list, clean and dedup
-        repo_dirs = Dir["#$top_dir/**/*.rpm"]
-        repo_dirs.map! { |item| item = File.dirname item }
-        repo_dirs.uniq!
+  # get repo dir list, clean and dedup
+  repo_dirs = Dir["#$top_dir/**/*.rpm"]
+  repo_dirs.map! { |item| item = File.dirname item }
+  repo_dirs.uniq!
 
-        for dir in repo_dirs
-                $logger.info("scanning repo #{dir}")
-                system "createrepo --update #{dir}"
-                if $?.exitstatus != 0
-                        $logger.error("Could not update repo #{dir}")
-                end
-        end
+  for dir in repo_dirs
+    $logger.info("scanning repo #{dir}")
+    system "createrepo --update #{dir}"
+    if $?.exitstatus != 0
+      $logger.error("Could not update repo #{dir}")
+    end
+  end
 end
 
 notifier = INotify::Notifier.new
-notifier.watch($top_dir, :recursive, :close, :move, :delete) do |event|
-        if event.name.match(/^.*\.rpm$/)
-                $logger.info("rpm change detected ... running repo scan")
-                scanAndUpdate()
-        end
+notifier.watch($top_dir, :recursive, :close_write, :move, :delete) do |event|
+  if event.name.match(/^.*\.rpm$/)
+    $logger.info("rpm change detected ... running repo scan")
+    scan_and_update
+  end
 end
 
+## MAIN() ##
+scan_and_update # run an initial repo scan at startup
 notifier.run
