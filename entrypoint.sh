1ba90fcd0eba727399fcdc3c48f2e5ea80d2087c
Diego Gutierrez <dgutierrez1287@gmail.com>
2017-03-26 07:00:52 -0400

    Revert "MODIFIED: entrypoint.sh"
    This reverts commit e975b804fc314983c62b30e65d102aca582557c7.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mkdir /root/logs/repo-scanner
+mkdir /logs/repo-scanner
+mkdir /logs/nginx
+mkdir /logs/supervisord
 
-ruby /root/scan_repo.rb
+chown nginx:nginx /logs/nginx
+
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
